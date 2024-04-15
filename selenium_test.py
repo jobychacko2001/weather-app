b7c8cf2f3760585b346a62938de8529fff1445de
Joby Chacko <jobychacko2001@gmail.com>
2024-04-15 17:19:54 -0400

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -6,46 +6,33 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-
-# Create a WebDriver instance
+# Create a WebDriver instance with options set for Chromium
 options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--headless')  # Run in headless mode
+options.add_argument('--no-sandbox')  # Bypass OS security model
+options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource problems
+options.binary_location = '/usr/bin/chromium-browser'  # Path to Chromium
+
+# Setup Chrome driver to manage automatically
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 # Navigate to your web application
 driver.get('http://127.0.0.1:8000/')
 
-# # Find an input field and enter text
-
-# input_field.send_keys('test')
-
-# # Click a button
-# button = driver.find_element_by_id('button_id')
-# button.click()
-
-# # Wait for page to load or specific element to appear
-# driver.implicitly_wait(10)  # Wait for 10 seconds
-
-# # Assert that a specific element or text is present
-# assert 'expected_text' in driver.page_source
-
-
 # Find the input field by its ID and enter "Toronto"
-input_field = driver.find_element(By.ID,'id_city')
+input_field = driver.find_element(By.ID, 'id_city')
 input_field.clear()  # Clear any existing text
 input_field.send_keys('Toronto')
 
 # Click the "Get Weather" button
-button = driver.find_element(By.ID,'submit')
+button = driver.find_element(By.ID, 'submit')
 button.click()
 
 # Wait for the weather info to load
-time.sleep(5)  # Wait for 10 seconds (you may adjust this time as needed)
+time.sleep(5)  # Wait for 5 seconds (you may adjust this time as needed)
 
 # Find the weather info elements and assert their presence
-weather_info = driver.find_element(By.CLASS_NAME,'weather-info')
+weather_info = driver.find_element(By.CLASS_NAME, 'weather-info')
 assert weather_info.is_displayed(), "Weather info is not displayed"
 
 # Further assertions can be added to validate the weather information content if needed
