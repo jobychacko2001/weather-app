1c336247c5fc04f940716b61795a6abbad05de06
Joby Chacko <jobychacko2001@gmail.com>
2024-04-15 22:52:50 -0400

    Update test_functional.py

--- test_functional.py
+++ test_functional.py
@@ -1,6 +1,14 @@
 from django.test import TestCase
-class WeatherAppTest(TestCase):
-    def test_get_weather(self):
-        response = self.client.get('http://127.0.0.1:8000/') 
-        self.assertEqual(response.status_code, 200)       
-        self.assertIn(b'Toronto', response.content)
+
+class WeatherAppTestCase(TestCase):
+    def test_weather_form_submission(self):
+        
+        response = self.client.post('http://127.0.0.1:8000/', {'city': 'Toronto'})  
+
+    
+        self.assertEqual(response.status_code, 200)
+
+    
+        self.assertIn(b'Toronto', response.content)  
+
+
