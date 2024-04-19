66a36691bdc0877afaa51395571441df0e5316f7
Don Sebastian <109496822+donsebastian24@users.noreply.github.com>
2024-04-18 20:37:47 -0400

    Testing , test_functional.py

--- test_functional.py
+++ test_functional.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 class WeatherAppTestCase(TestCase):
     def test_weather_form_submission(self):
         
-        response = self.client.post('http://127.0.0.1:8000/', {'city': 'Toronto'})  
+        response = self.client.post('http://127.0.0.1:8000/', {'city': '1234567'})  
 
     
         self.assertEqual(response.status_code, 200)
